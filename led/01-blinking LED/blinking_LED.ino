ea047057b2792da665ba5fe463e1de664657f383
Jags Zian A. Sumalinog <jagsyanyan@gmail.com>
2026-07-25 18:52:42 +0800

    Add files via upload

---
+++ led/01-blinking LED/blinking_LED.ino
@@ -0,0 +1,16 @@
+int ledPin= 23;
+int delayTime = 1000;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(delayTime);
+  digitalWrite(ledPin, LOW);
+  delay(delayTime);
+
+
+}

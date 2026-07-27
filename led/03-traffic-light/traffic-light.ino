f648787d6efe23a59a41264324a09b444cae2337
Jags Zian A. Sumalinog <jagsyanyan@gmail.com>
2026-07-27 23:26:29 +0800

    Refactor traffic light code for LED control

--- led/03-traffic-light/traffic-light.ino
+++ led/03-traffic-light/traffic-light.ino
@@ -4,36 +4,38 @@ int blinkDelay = 1000;
 int delayON = 3000;
 
 void setup() {
-pinMode(23, OUTPUT);
-pinMode(22, OUTPUT);
+pinMode(greenLED, OUTPUT); 
+pinMode(redLED, OUTPUT);
 
 }
 
 void loop() {
   
+// Green LED stays on for 3 seconds
 digitalWrite(greenLED, HIGH);
 delay(delayON);
 digitalWrite(greenLED, LOW);
 
 
- for(int green = 1; green <=4; green++)
+ for(int green = 1; green <=4; green++) // Green LED flashes 3 times
  {
   digitalWrite(greenLED, green);
   delay(blinkDelay);
-  digitalWrite(greenLED, LOW);
+  digitalWrite(greenLED, LOW);  // Green LED stays off
   delay(blinkDelay);
  }
 
+// Red LED stays on for 3 seconds
 digitalWrite(redLED, HIGH);
 delay(delayON);
 digitalWrite(redLED, LOW);
 
-for(int red =1; red <=4; red++)
+for(int red =1; red <=4; red++) // Red LED flashes for 3 times
 {
   digitalWrite(redLED, red);
   delay(blinkDelay);
-  digitalWrite(redLED, LOW);
+  digitalWrite(redLED, LOW); // Red LED stays off
   delay(blinkDelay);
 }  
-digitalWrite(redLED, LOW);
+
 }

dde047400c229d658460b1cb17f608a472442cfc
Jags Zian A. Sumalinog <jagsyanyan@gmail.com>
2026-08-20 18:52:48 +0800

    Add files via upload

---
+++ 03-Push-Button/04-Button-Press-Counter/Button-Press-Counter.ino
@@ -0,0 +1,30 @@
+const int LED = 21;
+const int button = 18;
+int  pressCounter = 0;
+int buttonreleased = LOW;
+
+void setup() {
+pinMode(button, INPUT_PULLUP);
+pinMode(LED, OUTPUT);
+Serial.begin(9600);
+Serial.println(pressCounter);
+}
+
+void loop() {
+  int buttonpress = digitalRead(button);
+
+  if (buttonpress == 0 && buttonreleased == LOW)
+  {
+    pressCounter++;
+    Serial.println(pressCounter);
+    buttonreleased = HIGH;
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+  }
+ if (buttonpress == 1)
+ {
+  buttonreleased = LOW;
+  delay(50);
+ }
+}

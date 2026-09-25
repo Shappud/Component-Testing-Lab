01910ed7c1312d5e7a0f7acb5077f28e5d8fd09e
Jags Zian A. Sumalinog <jagsyanyan@gmail.com>
2026-09-25 23:09:15 +0800

    Refactor LED fading logic in fading_LED.ino

--- 01-led/02-fading LED/fading_LED.ino
+++ 01-led/02-fading LED/fading_LED.ino
@@ -2,22 +2,19 @@ int LED = 23;
 int delayTime = 10;
 
 void setup() {
-ledcAttach(23,5000, 8); //Define pwm
+  ledcAttach(23, 5000, 8); // Set GPIO 23 to PWM
 }
 
+void loop() {
+  // Fade the LED from off to full brightness
+  for (int fade = 0; fade <= 255; fade++) {
+    ledcWrite(LED, fade); // Increase brightness
+    delay(delayTime); // Wait before next step
+  }
 
-void loop() {\
-// LED fade in 
-for (int fade = 0;fade <= 255; fade++ )
-{
-  ledcWrite(LED, fade);
-  delay(delayTime);
-}
-
-// LED fade out
-for (int fade = 255; fade >= 0; fade--)
-{
-ledcWrite(LED, fade);
-  delay(delayTime);
-}
+  // Fade the LED from full brightness to off
+  for (int fade = 255; fade >= 0; fade--) {
+    ledcWrite(LED, fade); // Decrease brightness
+    delay(delayTime); // Wait before next step
+  }
 }
